e76ee034546b6ef186e5e800a07a9278b08f777f
Mariana Maia <96018595+maia526@users.noreply.github.com>
2022-07-27 17:23:27 -0300

    Update biblioteca.cpp

--- biblioteca.cpp
+++ biblioteca.cpp
@@ -1,6 +1,54 @@
 #include <stdio.h>
+struct Usuario{
+    int ocupado = 0;
+    char nome[200];
+    long long cpf;
+};
 
-void menu_cadastro_usuarios()
+void cadastrar_usuario(Usuario usuarios[])
+{
+    for (int i = 0; i < 100; i++)
+    {
+        if (usuarios[i].ocupado == 0)
+        {
+            usuarios[i].ocupado = 1;
+            printf("CPF: ");
+            scanf("%lld", &usuarios[i].cpf);
+            printf("Nome: ");
+            getchar();
+            gets(usuarios[i].nome);
+            
+            break;
+        }
+    }
+}
+
+void listar_usuarios(Usuario usuarios[])
+{
+    for (int i = 0; i < 100; i++)
+    {
+        if(usuarios[i].ocupado == 1)
+        {
+            printf("\n%s", usuarios[i].nome);
+            printf("\n%lld", usuarios[i].cpf);
+        }
+    }
+}
+
+void excluir_usuario(Usuario usuarios[])
+{
+    long long cpf_excluir;
+    printf("Digite o CPF: ");
+    scanf("%lld", &cpf_excluir);
+    
+    for (int i = 0; i < 100; i++)
+    {
+        if (usuarios[i].cpf == cpf_excluir)
+            usuarios[i].ocupado = 0;
+    }
+}
+
+void menu_cadastro_usuarios(Usuario usuarios[])
 {
     while(true)
     {
@@ -11,13 +59,13 @@ void menu_cadastro_usuarios()
     printf("\n3-Listar usuarios");
     printf("\n4-Voltar\n");
     scanf("%d", &entrada);
-
-    // if (entrada == 1)
-    //     cadastrar_usuario();
-    // if (entrada == 2)
-    //     excluir_usuario();
-    // if (entrada == 3)
-    //     listar_usuarios();
+    
+     if (entrada == 1)
+        cadastrar_usuario(usuarios);
+     if (entrada == 2)
+         excluir_usuario(usuarios);
+     if (entrada == 3)
+         listar_usuarios(usuarios);
      if (entrada == 4)
          break;
     }
@@ -69,7 +117,7 @@ void menu_cadastro_emprestimo_devolucao()
     }
 }
 
-void menu_principal()
+void menu_principal(Usuario usuarios[])
 {
     while(true)
     {
@@ -81,10 +129,10 @@ void menu_principal()
     printf("\n4-Fim\n");
     
     scanf("%d", &entrada);
-    bool continuar = true;
+
     if (entrada == 1)
     {
-        menu_cadastro_usuarios();
+        menu_cadastro_usuarios(usuarios);
     }
         
     if (entrada == 2)
@@ -106,5 +154,6 @@ void menu_principal()
 
 int main()
 {
-    menu_principal();
+    Usuario usuarios[100];
+    menu_principal(usuarios);
 }
