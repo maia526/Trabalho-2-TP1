b0eb4ea0ff403ee33ae195b63a6aada25696c974
Mariana Maia <96018595+maia526@users.noreply.github.com>
2022-07-27 16:10:41 -0300

    Update biblioteca.cpp

--- biblioteca.cpp
+++ biblioteca.cpp
@@ -1,101 +1,107 @@
 #include <stdio.h>
-void cadastrar_usuario()
+
+void menu_cadastro_usuarios()
 {
-
-}
-
-
-bool menu_cadastro_usuarios()
-{
+    while(true)
+    {
     int entrada;
     printf("\n---Menu cadastro usuario---");
     printf("\n1-Cadastrar usuario");
     printf("\n2-Excluir usuario");
     printf("\n3-Listar usuarios");
-    printf("\n4-Voltar");
+    printf("\n4-Voltar\n");
+    scanf("%d", &entrada);
 
-    if (entrada == 1)
-        cadastrar_usuario();
-    if (entrada == 2)
-        excluir_usuario();
-    if (entrada == 3)
-        listar_usuarios();
-    if (entrada == 4)
-        return true;
+    // if (entrada == 1)
+    //     cadastrar_usuario();
+    // if (entrada == 2)
+    //     excluir_usuario();
+    // if (entrada == 3)
+    //     listar_usuarios();
+     if (entrada == 4)
+         break;
+    }
 }
 
-bool menu_cadastro_livros()
+void menu_cadastro_livros()
 {
+    while(true)
+    {
     int entrada;
     printf("\n---Menu do Cadastro de Livros---");
     printf("\n1-Cadastrar livro");
     printf("\n2-Excluir livro");
     printf("\n3-Listar livros");
-    printf("\n4-Voltar");
+    printf("\n4-Voltar\n");
+    scanf("%d", &entrada);
 
-    if (entrada == 1)
-        cadastrar_livro();
-    if (entrada == 2)
-        excluir_livro();
-    if (entrada == 3)
-        listar_livros();
-    if (entrada == 4)
-        return true;
+    // if (entrada == 1)
+    //     cadastrar_livro();
+    // if (entrada == 2)
+    //     excluir_livro();
+    // if (entrada == 3)
+    //     listar_livros();
+     if (entrada == 4)
+         break;
+    }
 }
 
-bool menu_cadastro_emprestimo_devolucao()
+void menu_cadastro_emprestimo_devolucao()
 {
+    while(true)
+    {
     int entrada;
     printf("\n---Menu de Emprestimo e Devolucao---");
     printf("\n1-Emprestar livro");
     printf("\n2-Devolver livro");
     printf("\n3-Listar emprestimos");
-    printf("\n4-Voltar");
+    printf("\n4-Voltar\n");
+    scanf("%d", &entrada);
 
-    if (entrada == 1)
-        emprestar_livro();
-    if (entrada == 2)
-        devolve_livro();
-    if (entrada == 3)
-        listar_emprestimos();
-    if (entrada == 4)
-        return true;
+    //  if (entrada == 1)
+    //      emprestar_livro();
+    //  if (entrada == 2)
+    //      devolve_livro();
+    //  if (entrada == 3)
+    //      listar_emprestimos();
+     if (entrada == 4)
+         break;
+    }
 }
 
 void menu_principal()
 {
+    while(true)
+    {
     int entrada;
     printf("\n---Menu principal---");
     printf("\n1-Cadastro de usuarios");
     printf("\n2-Cadastro de livros");
     printf("\n3-Emprestimo/devolucao");
-    printf("\n4-Fim");
+    printf("\n4-Fim\n");
     
     scanf("%d", &entrada);
     bool continuar = true;
     if (entrada == 1)
     {
-        continuar = menu_cadastro_usuarios();
-        if (continuar == true)
-            menu_principal();
+        menu_cadastro_usuarios();
     }
         
     if (entrada == 2)
     {
-        continuar = menu_cadastro_livros();
-        if (continuar == true)
-            menu_principal();
+        menu_cadastro_livros();
+        
     }
 
     if (entrada == 3)
     {
-        continuar = menu_cadastro_emprestimo_devolucao();
-        if (continuar == true)
-            menu_principal();
+        menu_cadastro_emprestimo_devolucao();
+        
     }
 
     if (entrada == 4)
         break;
+    }
 }
 
 int main()
