70f2df5ae53670fbbc9acad5e6bc2a550e2ce021
Mariana Maia <96018595+maia526@users.noreply.github.com>
2022-08-01 13:04:16 -0300

    Add files via upload

--- biblioteca.cpp
+++ biblioteca.cpp
@@ -5,6 +5,7 @@ struct Usuario
     int ocupado = 0; // todos começam com ocupado = 0. se for 1, significa que há um usuário cadastrado naquela posição. se for 0 significa que aquela posição pode ser usada para cadastrar outro usuário
     int repetiu_nome = 0;
     int tem_emprestimo = 0;
+    char nome_livro[30];
     char nome[15];
     char sobrenome[15];
     long long cpf;
@@ -21,34 +22,46 @@ struct Livro
     int emprestado = 0;
 };
 
-void organiza_estrutura (Usuario usuarios[])
-{
-    int i, j, aux;
-    for(i = 0; i < 99; i++){
-        for(j = i + 1; j < 100; j++){
-            if( usuarios[i].ocupado > usuarios[j].ocupado ){
-                Usuario aux    = usuarios[i];
-                usuarios[i] = usuarios[j];
-                usuarios[j] = aux;
-            }
-        }
-    }
+void ordena_titulo(Livro livros[], int qtd) {
+   bool trocou = true;
+   int k = qtd-1;
+
+   while (k > 0 && trocou)
+   {
+     trocou = false;
+     for (int i = 0; i < k; i++) {
+
+       int resultado = stricmp(livros[i].titulo, livros[i+1].titulo);
+
+       if (resultado > 0) {
+         char aux[31];
+
+         strcpy(aux, livros[i+1].titulo);            // aux = palavras[i+1]
+         strcpy(livros[i+1].titulo, livros[i].titulo);    // palavras[i+1] = palavras[i]
+         strcpy(livros[i].titulo, aux);              // palavras[i] = aux
+         trocou = true;
+       }
+     }
+     k--;
+   }
 }
 
 void ordena_cpf(Usuario usuarios[], int qtd_usuarios)
 {
     int i, j, aux;
-    for( i=0; i< qtd_usuarios - 1; i++ ){
-        for( j=i+1; j < qtd_usuarios; j++ ){
-            if( usuarios[i].cpf > usuarios[j].cpf){
-                Usuario aux    = usuarios[i];
+    for (i = 0; i < qtd_usuarios - 1; i++)
+    {
+        for (j = i + 1; j < qtd_usuarios; j++)
+        {
+            if (usuarios[i].cpf > usuarios[j].cpf)
+            {
+                Usuario aux = usuarios[i];
                 usuarios[i] = usuarios[j];
                 usuarios[j] = aux;
             }
         }
     }
 }
-
 
 void cadastrar_usuario(Usuario usuarios[])
 {
@@ -84,24 +97,22 @@ void cadastrar_usuario(Usuario usuarios[])
 
             for (int j = 0; j < 1;)
             {
-                
+
                 printf("Nome: ");
                 scanf(" %s", usuarios[i].nome);
                 strupr(usuarios[i].nome);
                 printf("Sobrenome: ");
                 scanf(" %s", usuarios[i].sobrenome);
                 strupr(usuarios[i].sobrenome);
-                
 
                 if (strlen(usuarios[i].nome) >= 4 && strlen(usuarios[i].sobrenome) >= 1)
                     j++;
                 else
                     printf("Nome invalido.\n");
-                    
             }
 
             usuarios[i].ocupado = 1;
-            
+
             break;
         }
     }
@@ -150,13 +161,13 @@ void excluir_usuario(Usuario usuarios[])
         {
             for (int i = 0; i < 100; i++)
             {
-                //esse tem.emprestimo é para verificar se o usuario tem algum emprestimo em andamento. se tiver não pode deletar. caso dê problema com isso mais para a frente é aqui que tem que ver provavelmente
+                // esse tem.emprestimo é para verificar se o usuario tem algum emprestimo em andamento. se tiver não pode deletar. caso dê problema com isso mais para a frente é aqui que tem que ver provavelmente
                 if (usuarios[i].cpf == cpf_excluir)
                     if (usuarios[i].tem_emprestimo == 0)
                         usuarios[i].ocupado = 0;
-                        k++;
-                    if (usuarios[i].tem_emprestimo == 1)
-                        printf("\nUsuario tem emprestimo em andamento.\n");
+                k++;
+                if (usuarios[i].tem_emprestimo == 1)
+                    printf("\nUsuario tem emprestimo em andamento.\n");
             }
         }
         if (tem == false)
@@ -201,7 +212,7 @@ void cadastrar_livro(Usuario usuarios[], Livro livros[])
 
             for (int j = 0; j < 1;)
             {
-                
+
                 printf("Nome do autor: ");
                 scanf(" %s", livros[i].nome);
                 strupr(livros[i].nome);
@@ -213,12 +224,11 @@ void cadastrar_livro(Usuario usuarios[], Livro livros[])
                     j++;
                 else
                     printf("Nome invalido.\n");
-                    
             }
 
-            for (int j = 0; j < 1; )
+            for (int j = 0; j < 1;)
             {
-                
+
                 printf("Titulo (sem espaços): ");
                 scanf(" %s", livros[i].titulo);
                 strupr(livros[i].titulo);
@@ -227,10 +237,9 @@ void cadastrar_livro(Usuario usuarios[], Livro livros[])
                     j++;
                 if (strlen(livros[i].titulo) < 4)
                     printf("Titulo invalido.\n");
-                    
             }
 
-            for (int j = 0; j < 1; )
+            for (int j = 0; j < 1;)
             {
                 printf("Ano de publicacao: ");
                 scanf("%d", &livros[i].ano);
@@ -242,8 +251,71 @@ void cadastrar_livro(Usuario usuarios[], Livro livros[])
             }
 
             livros[i].ocupado = 1;
-            
+
             break;
+        }
+    }
+}
+
+void excluir_livro(Usuario usuarios[], Livro livros[])
+{
+    int codigo_excluir;
+    bool tem = false;
+    bool emprestado = true;
+    printf("Digite o codigo: ");
+    scanf("%d", &codigo_excluir);
+
+    for (int k = 0; k < 1; k++)
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            if (livros[i].ocupado == 1)
+            {
+                if (codigo_excluir == livros[i].codigo)
+                    tem = true;
+                if (livros[i].emprestado == 0)
+                    emprestado = false;
+            }
+        }
+
+        if (tem == true)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                // esse tem.emprestimo é para verificar se o usuario tem algum emprestimo em andamento. se tiver não pode deletar. caso dê problema com isso mais para a frente é aqui que tem que ver provavelmente
+                if (emprestado == false)
+                    if (usuarios[i].tem_emprestimo == 0)
+                        usuarios[i].ocupado = 0;
+                k++;
+                if (emprestado == true)
+                    printf("\nLivro esta emprestado, nao e possivel exclui-lo.\n");
+            }
+        }
+        if (tem == false)
+        {
+            printf("\nCodigo nao consta no sistema\n");
+        }
+    }
+}
+
+void listar_livros(Usuario usuarios[], Livro livros[])
+{
+    int qtd_livros = 0;
+    for (int i = 0; i < 100; i++)
+    {
+        if (livros[i].ocupado == 1)
+            qtd_livros++;
+    }
+    ordena_titulo(livros, qtd_livros);
+
+    for (int i = 0; i < 100; i++)
+    {
+        if (livros[i].ocupado == 1)
+        {
+            printf("\n%s\n", livros[i].titulo);
+            printf(" %s %s", livros[i].nome, livros[i].sobrenome);
+            printf("\n%d\n", livros[i].codigo);
+            printf("\n%d", livros[i].ano);
         }
     }
 }
@@ -285,10 +357,10 @@ void menu_cadastro_livros(Usuario usuarios[], Livro livros[])
 
         if (entrada == 1)
             cadastrar_livro(usuarios, livros);
-        // if (entrada == 2)
-        //     excluir_livro();
-        // if (entrada == 3)
-        //     listar_livros();
+        if (entrada == 2)
+            excluir_livro(usuarios, livros);
+        if (entrada == 3)
+            listar_livros(usuarios, livros);
         if (entrada == 4)
             break;
     }
